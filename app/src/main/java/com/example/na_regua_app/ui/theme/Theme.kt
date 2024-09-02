73768541b661e9b04dfbecbb3b06cd5839c2ec09
melissaSNeves <melissa.neves@sptech.school>
2024-09-01 22:57:07 -0300

    Notificações finalizada

--- app/src/main/java/com/example/na_regua_app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/na_regua_app/ui/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -20,21 +19,13 @@ private val LightColorScheme = lightColorScheme(
     primary = BLUE_PRIMARY,
     secondary = ORANGE_SECUNDARY,
     tertiary = WHITE_BACKGROUND,
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-
 )
 
 @Composable
 fun NareguaappTheme(
     darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
